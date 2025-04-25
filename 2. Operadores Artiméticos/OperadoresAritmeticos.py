a8a51437c57c4cbcf66fff41950c5f7ad949e552
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-25 14:13:46 -0600

    se crea variables aritmeticas

---
+++ 2. Operadores Artiméticos/OperadoresAritmeticos.py
@@ -0,0 +1,20 @@
+##Suma
+print("Suma:",2 + 2)
+
+##Resta
+print("Resta:",150 - 75)
+
+##Multiplicación
+print("Multiplicación:",10 * 10)
+
+##División
+print("División:",75 / 9)
+
+##División Entera
+print("División entera:",75 // 9)
+
+##Ponencia 2x2x2
+print("Ponencia:",2 ** 3)
+
+##Módulo muestra el residuo
+print("Módulo:",100 % 100)
