8a218e5953e1e8dee81994332288febe0b594f7e
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-28 14:52:56 -0600

    Ejercicio con cadenas

---
+++ 3. Cadenas de Texto/Ejercicio1.py
@@ -0,0 +1,12 @@
+cadena = "Te quiero solo como amigo"
+
+print(cadena[0 : 2])#Primeros 2 caracteres
+
+#Cuando necesitas invertir empieza el conteo de 1
+print(cadena[-3: ])#El final de la cadena
+
+print(cadena[ : : 2])#Omite cada 2 caracteres
+
+print(cadena[ : : -1])#Invierte la cadena de texto
+
+print(cadena + " / " + cadena[ : : -1])
