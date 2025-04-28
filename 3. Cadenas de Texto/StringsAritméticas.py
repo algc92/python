344cb3568efc49da569eb43fe9eab5e55bcddb41
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-28 13:22:04 -0600

    Clase String con operadores de concatenación

---
+++ 3. Cadenas de Texto/StringsAritméticas.py
@@ -0,0 +1,9 @@
+cadena = "Hola Leonardo"
+cadena2 = "García"
+numero = 2
+
+print(cadena + " " + cadena2) #concatenación con el signo más(+) o signo(,)
+
+print(cadena2 * 5) #Es como realizar un for
+
+print(type(str(numero)))
