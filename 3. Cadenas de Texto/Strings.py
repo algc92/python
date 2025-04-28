c2477dbd660d8dffeabdc67113468d2efabe502a
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-28 13:14:14 -0600

    se crea clase String

---
+++ 3. Cadenas de Texto/Strings.py
@@ -0,0 +1,9 @@
+cadena = "Este es un \"ejemplo\" de cadena de texto"
+
+cadena2 = "Este es un \nejemplo de \tcadena de texto"
+
+cadena3 = "Este es un \bejemplo de cadena de texto" #existe \r \v
+
+print(cadena)
+print(cadena2)
+print(cadena3)
