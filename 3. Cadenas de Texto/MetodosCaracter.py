e41ef52352a3db6ab029508ebe79e099dd2d5ecb
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-28 14:38:11 -0600

    metodosCaracter

---
+++ 3. Cadenas de Texto/MetodosCaracter.py
@@ -0,0 +1,12 @@
+cadena = "Estoy utilizando los metodos de Python"
+
+##Métodos para cadenas
+print("Cadena en minuscula:",cadena.lower())#minusculas
+print("\n")
+print("Cadena en mayuscula:",cadena.upper())#mayusculas
+print("\n")
+print("Cadena capitalize:",cadena.capitalize())#primer letra en mayuscula
+print("\n")
+print("Cadena title:",cadena.title())#toma la primer letra de cada letra y la hace mayuscula
+print("\n")
+print("Cadena swapCase:",cadena.swapcase())#invierte de minusculas a mayusculas
