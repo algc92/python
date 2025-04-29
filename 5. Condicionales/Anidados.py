ab510b54c7fc304ace5e89f8bad742471a076e80
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-29 13:57:11 -0600

    clase Anidados

---
+++ 5. Condicionales/Anidados.py
@@ -0,0 +1,10 @@
+nombre = "Leonardo"
+edad = 20
+
+if nombre == "Leonardo":
+    if edad >= 18:
+        print("Tu eres Leo y eres mayor de edad")
+    else:
+        print("Eres Leonardo, pero no eres mayor de edad")
+else :
+    print("No eres Leonardo")
