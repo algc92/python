5ab765f69a231d33677b93e262e6b8929eccc60a
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-29 12:13:11 -0600

    se crea elif

---
+++ 5. Condicionales/Elif.py
@@ -0,0 +1,14 @@
+letra = input("Ingresa una vocal\n")
+
+if letra.lower() == "a":
+    print("Esta vocal es A")
+elif letra.lower() == "e":
+    print("Esta vocal es E")
+elif letra.lower() == "i":
+    print("Esta vocal es I")
+elif letra.lower() == "o":
+    print("Esta vocal es O")
+elif letra.lower() == "u":
+    print("Esta vocal es U")
+else :
+    print("No es una vocal")
