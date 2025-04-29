9fd063f02e9724b10a42a3a974964ce015f4885e
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-04-29 10:48:09 -0600

    Booleanos y condicionales

---
+++ 4. Booleanos/MetodosBooleanos.py
@@ -0,0 +1,8 @@
+cadena = "Estoy mostrando los métodos booleanos para las Strings"
+cadena2 = "HOLA MUNDO"
+
+print(cadena.startswith("E"))
+print(cadena.endswith("s"))
+
+print(cadena2.isupper())
+print(cadena.islower())
