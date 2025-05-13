1976b5336444f02ee17669d7ed7ec759dfa1dc5a
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-05-13 10:24:57 -0600

    funciones matemáticas

---
+++ 12. Funciones/FuncionesMatematicas.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+# Elevar al cuadrado los parametros(número y exponente)
+print("Elevar al cuadrado:",math.pow(10, 2))
+# Raiz cuadrada flotante
+print("Raiz cuadrada flotante:",math.sqrt(64))
+# Raiz cuadrada entero
+print("Raiz cuadrada entero:",math.isqrt(64))
+# obtener seno trigonometrico
+print("Seno:",math.sin(90))
+# obtener coseno trigonometrico
+print("Coseno:",math.cos(90))
+# obtener tangente trigonometrico
+print("Tangente:",math.tan(90))
+# obtener factorial
+print("Factorial:",math.factorial(5))
+# Número aleatorio
+print("Valor aleatorio:",random.randint(1, 100))
