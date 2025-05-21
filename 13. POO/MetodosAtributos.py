ae60bde8f88fbdb27fcb838367542e53874d85a2
Andrés Leonardo García Castillo <andres.garcia@tfja.gob.mx>
2025-05-21 10:56:12 -0600

    metodos y atributos

---
+++ 13. POO/MetodosAtributos.py
@@ -0,0 +1,22 @@
+class FabricaTelefonos():
+    marca = "Samsung"
+    color = "Negro"
+    mram = 32
+    almacenamiento = 128
+    
+    # todo método instancia debe contener el parametro self
+    def llamar(self, mensaje):
+        return mensaje
+
+    def escucharMusica(self):
+        print("Estas escuchando música")
+
+telefono = FabricaTelefonos()
+print(telefono.marca)
+print(telefono.color)
+print(telefono.mram)
+print(telefono.almacenamiento)
+
+# Aqui se esta mostrando el método y se pasa como parametro el mensaje
+print(telefono.llamar("Hola desde el método"))
+telefono.escucharMusica()
